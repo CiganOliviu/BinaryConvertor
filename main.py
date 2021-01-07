56ef8675095d02381e814c33be92f6faae387f77
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2021-01-07 21:14:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,15 +14,37 @@ def convert_decimal_to_binary(number):
 
     result.reverse()
 
-    return convert_list_to_number(result)
+    return result
+
+
+def convert_binary_to_decimal(binary_number):
+
+    binary_number_as_list = [int(item) for item in str(binary_number)]
+    binary_number_as_list.reverse()
+
+    result = 0
+
+    for index in range(len(binary_number_as_list)):
+        if binary_number_as_list[index] == 1:
+            result += (pow(2, index))
+
+    return result
 
 
 def test_convert_decimal_to_binary_():
 
-    assert convert_decimal_to_binary(1234) == 10011010010
-    assert convert_decimal_to_binary(4321) == 1000011100001
-    assert convert_decimal_to_binary(5) == 101
+    assert convert_list_to_number(convert_decimal_to_binary(1234)) == 10011010010
+    assert convert_list_to_number(convert_decimal_to_binary(4321)) == 1000011100001
+    assert convert_list_to_number(convert_decimal_to_binary(5)) == 101
+
+
+def test_convert_binary_to_decimal_():
+
+    assert (convert_binary_to_decimal(10011010010)) == 1234
+    assert (convert_binary_to_decimal(1000011100001)) == 4321
+    assert (convert_binary_to_decimal(101)) == 5
 
 
 if __name__ == '__main__':
     test_convert_decimal_to_binary_()
+    test_convert_binary_to_decimal_()
